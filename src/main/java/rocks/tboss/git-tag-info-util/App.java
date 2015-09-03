f519457d440827dc0f7f0f3bb29c48302c3f53f9
Tavish Fraser <tavishf@gmail.com>
2015-09-03 20:33:52 +1200

    Ridiculous maven quickstart skeleton.

---
+++ src/main/java/rocks/tboss/git-tag-info-util/App.java
@@ -0,0 +1,13 @@
+package rocks.tboss.git-tag-info-util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
